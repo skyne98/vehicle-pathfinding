29e283a733dc3a5ca2ddf464c97180354e64a0ab
raspbfox <ahalahan@gmail.com>
2024-03-08 06:47:57 +0100

    feat: work in progress vehicle based pathfinding

--- src/agent.rs
+++ src/agent.rs
@@ -1,4 +1,113 @@
+use notan::{
+    app::Color,
+    draw::{Draw, DrawShapes, DrawTransform},
+    math::{Affine2, Mat3, Vec2},
+};
+
 pub struct Agent {
-    pub position: (i32, i32),
-    pub size: (i32, i32),
+    pub position: Vec2,
+    pub size: Vec2,
+    /// Rotation in radians.
+    pub rotation: f32,
+}
+
+impl Agent {
+    pub fn new(position: Vec2, size: Vec2) -> Self {
+        Self {
+            position,
+            size,
+            rotation: 0.0,
+        }
+    }
+
+    pub fn footprint(&self, position: Vec2, rotation: f32) -> Vec<Vec2> {
+        let half_width = self.size.x / 2.0;
+        let half_height = self.size.y / 2.0;
+        let (x, y) = (position.x, position.y);
+        let corners = [
+            Vec2::new(
+                -half_width,
+                -half_height, // top-left
+            ),
+            Vec2::new(
+                half_width,
+                -half_height, // top-right
+            ),
+            Vec2::new(
+                half_width,
+                half_height, // bottom-right
+            ),
+            Vec2::new(
+                -half_width,
+                half_height, // bottom-left
+            ),
+        ];
+
+        let transform = Affine2::from_translation(Vec2::new(x, y)) * Affine2::from_angle(rotation);
+        let corners_transformed: Vec<Vec2> = corners
+            .iter()
+            .map(|corner| transform.transform_point2(*corner))
+            .collect();
+
+        let min_x = corners_transformed
+            .iter()
+            .map(|corner| corner.x)
+            .fold(f32::INFINITY, |acc, x| x.min(acc));
+        let min_y = corners_transformed
+            .iter()
+            .map(|corner| corner.y)
+            .fold(f32::INFINITY, |acc, y| y.min(acc));
+        let max_x = corners_transformed
+            .iter()
+            .map(|corner| corner.x)
+            .fold(f32::NEG_INFINITY, |acc, x| x.max(acc));
+        let max_y = corners_transformed
+            .iter()
+            .map(|corner| corner.y)
+            .fold(f32::NEG_INFINITY, |acc, y| y.max(acc));
+
+        let mut footprint = Vec::new();
+        for x in (min_x as i32)..=(max_x as i32) {
+            for y in (min_y as i32)..=(max_y as i32) {
+                footprint.push(Vec2::new(x as f32, y as f32));
+            }
+        }
+
+        footprint
+    }
+    pub fn current_footprint(&self) -> Vec<Vec2> {
+        self.footprint(self.position, self.rotation)
+    }
+
+    pub fn draw(&mut self, draw: &mut Draw, color: Color, cell_size: f32) {
+        let (x_grid, y_grid) = (self.position.x * cell_size, self.position.y * cell_size);
+        let (width_grid, height_grid) = (self.size.x * cell_size, self.size.y * cell_size);
+
+        let half_width = width_grid / 2.0;
+        let half_height = height_grid / 2.0;
+
+        let transform = Affine2::from_translation(Vec2::new(x_grid, y_grid))
+            * Affine2::from_angle(self.rotation)
+            * Affine2::from_translation(-Vec2::new(half_width, half_height));
+        draw.rect((0.0, 0.0), (width_grid, height_grid))
+            .color(color)
+            .transform(transform.into());
+
+        // draw the front
+        draw.line(
+            (half_width, half_height),
+            (half_width + half_width, half_height),
+        )
+        .color(Color::YELLOW)
+        .transform(transform.into());
+    }
+    pub fn draw_current_footprint(&mut self, draw: &mut Draw, color: Color, cell_size: f32) {
+        for footprint in self.current_footprint() {
+            draw.rect(
+                (footprint.x * cell_size, footprint.y * cell_size),
+                (cell_size, cell_size),
+            )
+            .color(color);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use std::hash::Hash;
+use std::time::Instant;
+
 use agent::Agent;
 use notan::draw::*;
 use notan::math::Vec2;
 use notan::prelude::*;
+use pathfinding::directed::astar::astar;
 
 pub mod agent;
 
@@ -11,7 +15,7 @@ struct State {
     grid: Grid,
     agent: Agent,
     mouse_pos: (f32, f32),
-    path: Option<Vec<(i32, i32)>>,
+    path: Option<Vec<Action>>,
 }
 
 struct Grid {
@@ -19,7 +23,6 @@ struct Grid {
     size: (i32, i32),
     cells: Vec<bool>,
 }
-
 impl Grid {
     fn new(cell_size: f32, width: i32, height: i32) -> Self {
         let size = (width / cell_size as i32, height / cell_size as i32);
@@ -42,12 +45,125 @@ impl Grid {
     }
 
     fn is_cell_blocked(&self, x: i32, y: i32) -> bool {
+        if x < 0 || x >= self.size.0 || y < 0 || y >= self.size.1 {
+            return true;
+        }
         self.cells[self.index(x, y)]
     }
 
     fn toggle_cell(&mut self, x: i32, y: i32) {
         let index = self.index(x, y);
         self.cells[index] = !self.cells[index];
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Action {
+    movement: f32,
+    rotation: f32,
+
+    start: Vec2,
+    start_rotation: f32,
+}
+impl Action {
+    pub fn new(movement: f32, rotation: f32, start: Vec2, start_rotation: f32) -> Self {
+        Self {
+            movement,
+            rotation,
+            start,
+            start_rotation,
+        }
+    }
+    pub fn end(&self) -> Vec2 {
+        self.start + Vec2::from_angle(self.start_rotation + self.rotation) * self.movement
+    }
+    pub fn end_rotation(&self) -> f32 {
+        self.start_rotation + self.rotation
+    }
+    pub fn neighbors(
+        &self,
+        move_increments: f32,
+        move_limit: f32,
+        turn_increments: f32,
+        turn_limit: f32,
+    ) -> Vec<Self> {
+        let mut neighbors = Vec::new();
+        let mut turn = 0.0;
+        let mut movement;
+        while turn <= turn_limit {
+            movement = 0.0;
+            while movement <= move_limit {
+                // positive
+                let turn_abs = turn.abs();
+                let turn_fraction = turn_abs / turn_limit;
+                let turn_fraction = turn_fraction * turn_fraction * turn_fraction;
+                let actual_movement = movement * (1.0 - turn_fraction);
+                neighbors.push(Self::new(
+                    actual_movement,
+                    turn,
+                    self.end(),
+                    self.end_rotation(),
+                ));
+                neighbors.push(Self::new(
+                    actual_movement,
+                    -turn,
+                    self.end(),
+                    self.end_rotation(),
+                ));
+                neighbors.push(Self::new(
+                    -actual_movement,
+                    turn,
+                    self.end(),
+                    self.end_rotation(),
+                ));
+                neighbors.push(Self::new(
+                    -actual_movement,
+                    -turn,
+                    self.end(),
+                    self.end_rotation(),
+                ));
+
+                movement += move_increments;
+            }
+            turn += turn_increments;
+        }
+
+        neighbors
+    }
+    pub fn cost(&self) -> u32 {
+        1
+    }
+
+    pub fn draw(&self, draw: &mut Draw, font: &Font, cell_size: f32) {
+        let color = if self.movement.signum() > 0.0 {
+            Color::BLUE
+        } else {
+            Color::RED
+        };
+        draw_arrow(draw, self.start * cell_size, self.end() * cell_size, color);
+
+        // write the data
+        let text = format!("M: {:.2} R: {:.2}", self.movement, self.rotation);
+        let text_position = self.start + (self.end() - self.start) / 2.0 + Vec2::new(0.0, 0.5);
+        draw.text(&font, &text)
+            .translate(text_position.x * cell_size, text_position.y * cell_size)
+            .size(15.0)
+            .color(Color::WHITE);
+    }
+}
+impl PartialEq for Action {
+    fn eq(&self, other: &Self) -> bool {
+        let start_x = self.start.x as i32 == other.start.x as i32;
+        let start_y = self.start.y as i32 == other.start.y as i32;
+        let end_x = self.end().x as i32 == other.end().x as i32;
+        let end_y = self.end().y as i32 == other.end().y as i32;
+        start_x && start_y && end_x && end_y
+    }
+}
+impl Eq for Action {}
+impl Hash for Action {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        ((self.start.x) as u32, (self.start.y) as u32).hash(state);
     }
 }
 
@@ -71,85 +187,64 @@ fn setup(gfx: &mut Graphics) -> State {
     State {
         font,
         grid,
-        agent: Agent {
-            position: (0, 0),
-            size: (2, 2),
-        },
+        agent: Agent::new(Vec2::new(3.0, 3.0), Vec2::new(1.5, 0.75)),
         mouse_pos: (0.0, 0.0),
         path: None,
     }
 }
 
-fn pathfind(state: &mut State, to: (i32, i32)) {
-    let start_time = std::time::Instant::now();
-    let start = state.agent.position;
-    let grid = &state.grid;
-    let (agent_width, agent_height) = state.agent.size;
-    let result = pathfinding::directed::astar::astar(
-        &start,
-        |p| {
-            let mut neighbors = Vec::new();
-            let (x, y) = *p;
-            'outer: for (dx, dy) in &[
-                (-1, 0),
-                (1, 0),
-                (0, -1),
-                (0, 1),
-                (-1, -1),
-                (1, -1),
-                (-1, 1),
-                (1, 1),
-            ] {
-                let nx = x + dx;
-                let ny = y + dy;
-                if nx >= 0
-                    && nx < grid.size.0
-                    && ny >= 0
-                    && ny < grid.size.1
-                    && !grid.is_cell_blocked(nx, ny)
-                {
-                    let diagonal = dx.abs() + dy.abs() == 2;
-                    let cost = if diagonal { 14 } else { 10 };
+fn pathfind(state: &mut State, to: Vec2) {
+    let start = Instant::now();
+    let start_action = Action::new(0.0, 0.0, state.agent.position, state.agent.rotation);
 
-                    // check if the agent can fit in the cell
-                    if nx + agent_width > grid.size.0 || ny + agent_height > grid.size.1 {
-                        continue;
+    let result = astar(
+        &start_action,
+        |action| {
+            let to_check = action
+                .neighbors(
+                    0.1,
+                    1.0,
+                    std::f32::consts::PI / 128.0,
+                    std::f32::consts::PI / 4.0,
+                )
+                .into_iter()
+                .map(|neigh| (neigh.clone(), neigh.cost()))
+                .collect::<Vec<(Action, u32)>>();
+            to_check
+                .into_iter()
+                .filter(|(action, _)| {
+                    // out of bounds
+                    let (x, y) = (action.end().x as i32, action.end().y as i32);
+                    if x < 0 || x >= state.grid.size.0 || y < 0 || y >= state.grid.size.1 {
+                        println!("Out of bounds: {:?}", action.end());
+                        return false;
                     }
-                    for i in nx..nx + agent_width {
-                        for j in ny..ny + agent_height {
-                            if grid.is_cell_blocked(i, j) {
-                                continue 'outer;
-                            }
-                        }
-                    }
-
-                    // additional check for diagonal movement
-                    for i in 0..agent_width {
-                        for j in 0..agent_height {
-                            if grid.is_cell_blocked(nx + i, y + j) {
-                                continue 'outer;
-                            }
-                            if grid.is_cell_blocked(x + i, ny + j) {
-                                continue 'outer;
-                            }
-                        }
-                    }
-
-                    neighbors.push(((nx, ny), cost));
-                }
-            }
-            neighbors
+                    // blocked
+                    state.grid.is_cell_blocked(x, y) == false
+                })
+                .filter(|(action, _)| {
+                    let footprint = state.agent.footprint(action.end(), action.end_rotation());
+                    footprint
+                        .iter()
+                        .all(|cell| !state.grid.is_cell_blocked(cell.x as i32, cell.y as i32))
+                })
+                .collect::<Vec<(Action, u32)>>()
         },
-        |p| {
-            let (x, y) = *p;
-            let from = Vec2::new(x as f32, y as f32);
-            let to = Vec2::new(to.0 as f32, to.1 as f32);
-            ((to - from).length() * 10.0) as i32
+        |action| ((to - action.end()).length() * 10.0) as u32,
+        |action| {
+            let (x, y) = (action.end().x as i32, action.end().y as i32);
+            let (goal_x, goal_y) = (to.x as i32, to.y as i32);
+            x == goal_x && y == goal_y
         },
-        |p| *p == to,
     );
-    state.path = result.map(|(path, _)| path);
-    println!("Pathfinding took: {:?}", start_time.elapsed());
+
+    if let Some((path, _)) = result {
+        state.path = Some(path.iter().map(|a| a.clone()).collect());
+    } else {
+        state.path = None;
+    }
+
+    println!("Pathfinding took: {:?}", start.elapsed());
 }
 
 fn update(app: &mut App, state: &mut State) {
@@ -161,17 +256,14 @@ fn update(app: &mut App, state: &mut State) {
         state.grid.toggle_cell(grid_x, grid_y);
     }
     if app.mouse.was_pressed(MouseButton::Middle) {
-        state.agent.position = (
-            (x / state.grid.cell_size) as i32,
-            (y / state.grid.cell_size) as i32,
-        );
+        state.agent.position = Vec2::new(x / state.grid.cell_size, y / state.grid.cell_size);
     }
     if app.mouse.was_pressed(MouseButton::Right) {
         let to = (
             (x / state.grid.cell_size) as i32,
             (y / state.grid.cell_size) as i32,
         );
-        pathfind(state, to);
+        pathfind(state, Vec2::new(to.0 as f32, to.1 as f32));
     }
 }
 
@@ -187,6 +279,37 @@ fn draw_selection(draw: &mut Draw, position: (i32, i32), size: f32, color: Color
     rect(draw, x_grid, y_grid, line_width, size);
     rect(draw, x_grid + size - line_width, y_grid, line_width, size);
     rect(draw, x_grid, y_grid + size - line_width, size, line_width);
+}
+fn draw_arrow(draw: &mut Draw, from: Vec2, to: Vec2, color: Color) {
+    if from.is_finite() == false || to.is_finite() == false {
+        return;
+    }
+
+    let dir = to - from;
+    let length = dir.length();
+    let dir = dir.normalize();
+    let angle = dir.y.atan2(dir.x);
+    let arrow_size = 10.0;
+    let arrow_end = from + dir * length;
+    let arrow_start = arrow_end
+        - Vec2::new(arrow_size, 0.0).rotate(Vec2::from_angle(angle + std::f32::consts::PI / 6.0));
+    let arrow_end2 = arrow_end
+        - Vec2::new(arrow_size, 0.0).rotate(Vec2::from_angle(angle - std::f32::consts::PI / 6.0));
+
+    // check for NaN and infinite values
+    if arrow_start.is_finite() == false
+        || arrow_end.is_finite() == false
+        || arrow_end2.is_finite() == false
+    {
+        return;
+    }
+
+    draw.line((from.x, from.y), (arrow_end.x, arrow_end.y))
+        .color(color);
+    draw.line((arrow_end.x, arrow_end.y), (arrow_start.x, arrow_start.y))
+        .color(color);
+    draw.line((arrow_end.x, arrow_end.y), (arrow_end2.x, arrow_end2.y))
+        .color(color);
 }
 
 fn draw(gfx: &mut Graphics, state: &mut State) {
@@ -225,25 +348,14 @@ fn draw(gfx: &mut Graphics, state: &mut State) {
     }
 
     // Draw the agent
-    let (x, y) = state.agent.position;
-    let (agent_width, agent_height) = state.agent.size;
-    for x in x..x + agent_width {
-        for y in y..y + agent_height {
-            draw.rect(
-                (
-                    x as f32 * state.grid.cell_size,
-                    y as f32 * state.grid.cell_size,
-                ),
-                (state.grid.cell_size, state.grid.cell_size),
-            )
-            .color(Color::RED);
-        }
-    }
+    state
+        .agent
+        .draw(&mut draw, Color::RED, state.grid.cell_size);
 
     // Draw the path
     if let Some(path) = &state.path {
-        for cell in path {
-            draw_selection(&mut draw, *cell, state.grid.cell_size, Color::BLUE);
+        for action in path {
+            action.draw(&mut draw, &state.font, state.grid.cell_size);
         }
     }
 
